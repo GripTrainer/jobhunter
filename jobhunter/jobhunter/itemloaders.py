95d33f46bbba46658396502e6712d782c971e653
luke-falvey <lukebfalvey@gmail.com>
2019-05-22 16:00:42 +1200

    Add spider that crawls SEEK

---
+++ jobhunter/jobhunter/itemloaders.py
@@ -0,0 +1,11 @@
+import unicodedata
+
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, Join, TakeFirst
+
+
+class SeekLoader(ItemLoader):
+    text_in = MapCompose(lambda unicode_str: unicodedata.normalize("NFKD", unicode_str), str.strip)
+    text_out = Join()
+    default_input_processor = MapCompose(str.strip)
+    default_output_processor = TakeFirst()
---
+++ jobhunter/jobhunter/spiders/seek.py
@@ -0,0 +1,32 @@
+import scrapy
+import re
+
+from jobhunter.itemloaders import SeekLoader
+from jobhunter.items import SeekItem
+
+BASE_URL = 'https://www.seek.co.nz'
+
+class SeekSpider(scrapy.Spider):
+    name = 'seek'
+    start_urls = [
+        f'{BASE_URL}/jobs-in-information-communication-technology/in-All-Wellington?sortmode=ListedDate'
+    ]
+
+    def parse(self, response):
+        for job in response.css('a[href*="/job/"]'):
+            yield response.follow(job, callback=self.parse_job)
+
+        for page in response.css('p span a'):
+            yield response.follow(page)
+        
+
+    def parse_job(self, response):
+        sl = SeekLoader(item=SeekItem(), response=response)
+        sl.add_value('id', response.text, re=r'"id":(\d+)')
+        sl.add_value('classification', response.text, re=r'"jobSubClassification":"([^"]+)')
+        sl.add_value('listingdate', response.text, re=r'"listingDate":"([^"]+)"')
+        sl.add_value('expirydate', response.text, re=r'"expiryDate":"([^"]+)"')
+        sl.add_value('company', response.text, re=r'"advertiserName":"([^"]+)"')
+        sl.add_css('jobtitle', 'h1.jobtitle::text')
+        sl.add_css('text', 'p::text')
+        yield sl.load_item()
