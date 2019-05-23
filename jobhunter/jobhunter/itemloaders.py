4d85d376bbf6a221b23a8aedac8f0d51bd2c2913
luke-falvey <lukebfalvey@gmail.com>
2019-05-23 13:46:02 +1200

    Replace TrademeLoader with Generic Itemloader

--- jobhunter/jobhunter/itemloaders.py
+++ jobhunter/jobhunter/itemloaders.py
@@ -3,13 +3,14 @@ import unicodedata
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose, Join, TakeFirst
 
-
-class SeekLoader(ItemLoader):
-    text_in = MapCompose(lambda unicode_str: unicodedata.normalize("NFKD", unicode_str), str.strip)
+class JobAdloader(ItemLoader):
+    text_in = MapCompose(str.strip)
     text_out = Join()
     default_input_processor = MapCompose(str.strip)
     default_output_processor = TakeFirst()
 
-class TrademeLoader(ItemLoader):
-    text_out = Join()
-    default_output_processor = TakeFirst()
+
+class SeekLoader(JobAdloader):
+    text_in = MapCompose(lambda unicode_str: unicodedata.normalize("NFKD", unicode_str), str.strip)
+
+    
