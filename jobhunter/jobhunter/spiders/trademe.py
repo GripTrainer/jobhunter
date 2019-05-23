4a77e58000504fe37337095b2ddba8a6e05e6fbd
luke-falvey <lukebfalvey@gmail.com>
2019-05-23 13:46:18 +1200

    Update trademe spider to use JobAdLoader

--- jobhunter/jobhunter/spiders/trademe.py
+++ jobhunter/jobhunter/spiders/trademe.py
@@ -1,5 +1,5 @@
 from scrapy.spiders import SitemapSpider
-from jobhunter.itemloaders import TrademeLoader
+from jobhunter.itemloaders import JobAdloader
 from jobhunter.items import JobAd
 
 
@@ -11,13 +11,13 @@ class TrademeSpider(SitemapSpider):
     sitemap_rules = [('/jobs', 'parse_job')]
 
     def parse_job(self, response):
-        tl = TrademeLoader(item=JobAd(), response=response)
-        tl.add_value('id', response.text, re='"listingId": (\d+)')
-        tl.add_value('jobtitle', response.text, re='"title": "([^"]+)"')
-        tl.add_css('classification', '#jobs-breadcrumb-depth-2::text')
-        tl.add_value('listingdate', response.text, re=r'\w{3} \d{1,2} \w{3}, \d{1,2}:\d\d \w{2}')
-        tl.add_css('expirydate', '#JSocialTools_JExpireTimeLabel::text')
-        tl.add_value('company', response.text, re='"jobsCompany": "([^"]+)"')
-        tl.add_css('text', '#j-description-contents *::text')
-        yield tl.load_item()
+        jl = JobAdloader(item=JobAd(), response=response)
+        jl.add_value('id', response.text, re='"listingId": (\d+)')
+        jl.add_value('jobtitle', response.text, re='"title": "([^"]+)"')
+        jl.add_css('classification', '#jobs-breadcrumb-depth-2::text')
+        jl.add_value('listingdate', response.text, re=r'\w{3} \d{1,2} \w{3}, \d{1,2}:\d\d \w{2}')
+        jl.add_css('expirydate', '#JSocialTools_JExpireTimeLabel::text')
+        jl.add_value('company', response.text, re='"jobsCompany": "([^"]+)"')
+        jl.add_css('text', '#j-description-contents *::text')
+        yield jl.load_item()
 
