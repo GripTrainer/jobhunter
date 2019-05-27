1b41a4d283dafac668fe38859f15777435151e87
luke-falvey <lukebfalvey@gmail.com>
2019-05-27 15:26:07 +1200

    Add pipeline that emails me jobs with keywords

--- jobhunter/jobhunter/pipelines.py
+++ jobhunter/jobhunter/pipelines.py
@@ -1,11 +1,35 @@
-# -*- coding: utf-8 -*-
+import re
+import yagmail
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+class JobAlertPipeline(object):
+    def __init__(self, email, email_pass):
+        self.email = email
+        self.email_pass = email_pass
+        self.keywords = re.compile('|'.join(['python', 'aws', ' r ']))
+        self.jobs = []
 
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            email=crawler.settings['EMAIL'], 
+            email_pass=crawler.settings['EMAIL_PASS']
+        )
 
-class JobhunterPipeline(object):
     def process_item(self, item, spider):
+        if item.get('text') and self.keywords.search(item['text'].lower()):
+            self.jobs.append(item)
+
         return item
+
+    def close_spider(self, spider):
+        mailer = yagmail.SMTP(self.email, self.email_pass)
+        mailer.send(
+            to=self.email,
+            subject=f'Daily Job Hunt - {spider.name}', 
+            contents=[field for job in self.jobs for field in (job.get('jobtitle'), job.get('url'))]
+        )
+
+
+
+
+
